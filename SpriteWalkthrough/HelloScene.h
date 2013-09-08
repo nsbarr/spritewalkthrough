206af43bdb910f987112125b6d5774d9bed2d50c
nsbarrcanvas <nick@canv.as>
2013-09-08 17:49:00 -0400

    fuck

---
+++ SpriteWalkthrough/HelloScene.h
@@ -0,0 +1,13 @@
+//
+//  HelloScene.h
+//  SpriteWalkthrough
+//
+//  Created by Nicholas Barr on 9/8/13.
+//  Copyright (c) 2013 Nicholas Barr. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface HelloScene : SKScene
+
+@end
